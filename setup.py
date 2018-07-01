c80f6a8b32528349d0d95ebfab1b0cc79ab8c9d1
ysard <ysard@users.noreply.github.com>
2018-07-01 20:58:53 +0200

    Read metadata of setup.py directly from caspo module => Remove all useless code for the upload on pypi

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Caspo
 # Copyright (c) 2014-2016, Santiago Videla
 #
 # This file is part of caspo.
@@ -14,47 +18,24 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with caspo.  If not, see <http://www.gnu.org/licenses/>.
-# -*- coding: utf-8 -*-
 """Installation module for Caspo program."""
 
-import os
-import codecs
-import re
 from setuptools import setup, find_packages
+import caspo
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-def read(*parts):
-    """
-    Build an absolute path from *parts* and and return the contents of the
-    resulting file.  Assume UTF-8 encoding.
-    """
-    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f_d:
-        return f_d.read()
-
-META_FILE = read(os.path.join("caspo", "__init__.py"))
-
-def find_meta(meta):
-    """
-    Extract __*meta*__ from META_FILE.
-    """
-    meta_match = re.search(
-        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
-        META_FILE, re.M
-    )
-    if meta_match:
-        return meta_match.group(1)
-    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+def load_requirements():
+    """Load requirements.txt file and return a list of lines in this file"""
+    with open('requirements.txt') as f_d:
+        return [line.rstrip() for line in f_d]
 
 setup(
-    name=find_meta("package"),
-    version=find_meta("version"),
-    description=find_meta("description"),
-    author=find_meta("author"),
-    author_email=find_meta("email"),
-    url=find_meta("url"),
-    license=find_meta("license"),
-    long_description=read("README.md"),
+    name=caspo.__package__,
+    version=caspo.__version__,
+    description=caspo.__description__,
+    author=caspo.__author__,
+    author_email=caspo.__email__,
+    url=caspo.__url__,
+    long_description=open("README.md").read(),
     classifiers=[
         "Intended Audience :: Science/Research",
         "Intended Audience :: Healthcare Industry",
